5f38acd966757e17674855ef2a4fd674cc882b3b
Sagar Pandya <sagargp@gmail.com>
2013-12-03 01:10:14 -0700

    Deleted floating editDistance() function

--- automover2.py
+++ automover2.py
@@ -211,24 +211,3 @@ if __name__ == "__main__":
   d = Debug(verbose=True)
   a = Automover(debug=d)
   a.run()
-
-def editDistance(s1, s2):
-  from numpy import zeros
-
-  m = zeros([len(s1), len(s2)], int)
-  m[0, 0] = 0
-
-  for i in range(0, len(s1)):
-    m[i, 0] = i
-
-  for j in range(0, len(s2)):
-    m[0, j] = j
-
-  for i in range(1, len(s1)):
-    for j in range(1, len(s2)):
-      if s1[i - 1] == s2[j - 1]:
-        m[i, j] = m[i - 1, j - 1]
-      else:
-        m[i, j] = 1 + min(m[i, j - 1], m[i - 1, j], m[i - 1, j - 1])
-
-  return m[len(s1) - 1, len(s2) - 1]
