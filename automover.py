d9cf312c3a72d0d50c619fec7ba5d2b4d0d7b8eb
Sagar Pandya <sagargp@gmail.com>
2014-02-13 21:33:19 -0700

    Finally deleting old version

--- automover.py
+++ automover.py
@@ -1,194 +1,103 @@
-import pickle
 import re
 import os
-import sys
-import argparse
-import ConfigParser
-from time import ctime
-from editDistance import editDistance
-from EpGuidesSearch import EpGuidesSearch
+import logging
+from datetime import datetime
+from parsers import *
+from searchers import *
 
-class Debug:
-  def __init__(self, verbose):
-    self.verbose = verbose
+class Automover:
+  def __init__(self, search_path, config, searcher, debug):
+    self.search_path = os.path.abspath(search_path)
+    self.config      = config
+    self.searcher    = searcher
+    self.debug       = debug
+    self.debug.info("Initializing...")
 
-  def out(self, verbose, kind, string):
-    debug_message = "%s -- %s -- %s" % (ctime(), kind, string)
+    patterns = [p for p in self.config.options('patterns') if p.startswith('pattern')]
 
-    if self.verbose:
-      print debug_message
+    self.debug.info("Compiling regular expressions")
+    self.patterns = [re.compile(self.config.get('patterns', p), flags=re.IGNORECASE) for p in patterns]
+    self.alnum_pattern = re.compile('[\W_]+')
 
-  def info(self, string):
-    self.out(self.verbose, "INFO", string)
+    self.show_list = []
+    for show in os.listdir(self.config.get('main', 'destination')):
+      self.show_list.append(show)
+    self.debug.info("Saved list of %d shows" % len(self.show_list))
+    self.debug.info("Done initializing")
 
-  def warn(self, string):
-    self.out(self.verbose, "WARN", string)
+  def run(self):
+    files = self._get_files()
 
-  def __call__(self, *args, **kwargs):
-    self.info(*args, **kwargs)
+    for src, dest in files:
+      self._move(src, dest)
 
-class automover:
-  def __init__(self, args, debug, dictionary):
-    self.args = args
-    self.dictionary = dictionary
-    self.debug = debug
+  def _move(self, src, dest):
+    self.debug.info('move "%s" to "%s"' % (src, dest))
 
-  def getEpisodeTitle(self, name):
-    mindist = None
-    bestMatch = ""
-    for file in self.dictionary:
-      match = editDistance(name, file.lower().replace('the', ''))
-      if match < mindist or mindist is None:
-        mindist = match
-        bestMatch = file
-    return bestMatch, mindist
+  def _rename(self, show, season, episode, episode_title, extension):
+    return "%s S%02dE%02d %s.%s" % (show, season, episode, episode_title, extension)
 
-  def doRename(self, root, file):
-    # skip files that match the exclude pattern
-    if exc.search(file) is not None:
-      return None
-
-    # skip files that do not match the search pattern
-    search = None
-    for pattern in pattern_regs:
-      search = pattern.search(file)
-      if search is not None:
-        break
-    if search is None:
-      return None
+  def _get_files(self):
+    matched_files = []
+    self.debug.info("Finding files in %s" % self.search_path)
+    destination_root = os.path.abspath(self.config.get('main', 'destination'))
     
-    # extract media information from the filename
-    groups = search.groups()
+    for root, subs, files in os.walk(self.search_path):
+      for filename in files:
+        match = self._match(filename)
+        if match:
+          # extract info from the regex match
+          groups      = match.groups()
+          show        = self._get_show_name(groups[0])
+          season      = int(groups[1].lstrip("0"))
+          episode     = int(groups[2].lstrip("0"))
 
-    title = groups[0].replace('.', ' ').lower().replace('the', '').strip()
-    season = groups[1].lstrip('0')
-    episode = groups[2].lstrip('0')
+          # lookup the episode title online if needed
+          ep_title    = self.searcher.get_episode_name(show, season, episode)
+          source_path = os.path.join(root, filename)
 
-    # use a cache to avoid repeat online searches
-    if title in titlecache:
-      bestMatch = titlecache[title]
-      dist = 0
-    else:
-      bestMatch, dist = self.getEpisodeTitle(title)
-      titlecache[title] = bestMatch
+          self.debug.info("%s is %s Season %s Episode %s %s" % (filename, show, season, episode, ep_title))
 
-    if bestMatch in showcache:
-      show = showcache[bestMatch]
-    else:
-      show = EpGuidesSearch(bestMatch, self.debug)
-      showcache[bestMatch] = show
+          # construct the new name
+          extension   = filename.split('.')[-1]
+          new_title = self._rename(show, season, episode, ep_title, extension)
 
-    result = show.search(season, episode)
+          # find the full path of the new destination
+          destination = os.path.join(destination_root, show, new_title)
 
-    if len(result) == 0 or dist > 3:
-      self.debug.warn("No results for %s Season %s episode %s" % (bestMatch, season, episode))
-      return None
-    else:
-      newname     = "%s S%02dE%02d %s.%s" % (bestMatch, int(season), int(episode), result[0]['title'].strip('"'), groups[-1])
-      destpath    = "%s/%s/Season %s" % (dest, bestMatch, season)
-      destination = "%s/%s" % (destpath, newname)
+          # save the src and dest into a list for return later
+          matched_files.append((source_path, destination))
+    return matched_files
 
-    fullpath = '/'.join([root, file])
+  def _match(self, filename):
+    for pattern in self.patterns:
+      search = pattern.search(filename)
+      if search is not None:
+        return search
+    return None
 
-    return bestMatch, fullpath, destpath, destination
-
-## Main
-if __name__ == '__main__':
-  argparser = argparse.ArgumentParser(description="Automatically rename and move TV shows")
-  argparser.add_argument("searchpath", nargs=1, help="The file or directory to process")
-  argparser.add_argument("--conf", nargs=1, default="/etc/automover.conf", help="Path to the config file")
-  argparser.add_argument("--read", action="store_true", help="Take filenames from STDIN") 
-  argparser.add_argument("--script", nargs="?", default="automover.sh", help="Write a bash script")
-  argparser.add_argument("--verbose", action="store_true", help="Verbose output") 
-  argparser.add_argument("--hint", nargs="+", help="Give a hint to the file name, if it isn't in the destination location")
-  argparser.add_argument("--logfile", nargs="?", default="automover.log", help="Debug logfile")
-  args = argparser.parse_args(sys.argv[1:])
-
-
-  # parse the config file and grab settings from it
-  config = ConfigParser.RawConfigParser()
-  config.read(args.conf)
-
-  patterns     = [x for x in config.options('patterns') if x.startswith('pattern')]
-  dest         = config.get('main', 'destination')
-  pattern_regs = [re.compile(config.get('patterns', x), flags=re.IGNORECASE) for x in patterns]
-  exc          = re.compile(config.get('patterns', 'exclude'), flags=re.IGNORECASE)
-
-  #
-  path = args.searchpath[0]
-
-  # Build a "dictionary" of TV show names. This is used later to look up names.
-  dictionary = [f for f in os.listdir(dest)]
-
-  # Add the hint as a key in the dictionary so that the algorithm will find it for sure
-  if args.hint is not None:
-    dictionary.append(' '.join(args.hint))
-
-  # initialize the debug printer
-  debug = Debug(args.verbose)
-
-  # initialize the automover object
-  automover = automover(args, debug, dictionary)
-
-  # prepare the caches
-  try:
-    cachefile = open('caches.pyo', 'r')
-    showcache, titlecache = pickle.load(cachefile)
-    cachefile.close()
-  except:
-    debug('Initializing caches')
-    showcache = dict()
-    titlecache = dict()
-  destpathcache = dict()
-
-  # prepare the list of files to rename
-  filelist = []
-  if args.read:
-    # either from stdin
-    for file in sys.stdin.read().splitlines():
-      root = os.path.dirname(file)
-      filename = os.path.basename(file)
-      filelist.append((root, filename))
-  else:
-    # or from the path given
-    for root, subs, files in os.walk(path):
-      for file in files:
-        filelist.append((root, file))
+  def _get_show_name(self, name):
+    name = name.replace(".", " ").lower().replace("the", "").strip()
+    for show in self.show_list:
+      s = show.lower().replace("the", "").replace(",", "").strip()
+      if name.startswith(s):
+        return show
+    return None
   
-  shows = dict()
-  for root, file in filelist:
-    move = automover.doRename(root, file)
-    debug(move)
+if __name__ == "__main__":
+  import pickle
 
-    if move is not None:
-      title, fullpath, destpath, destination = move
+  try:
+    with open("cache_file.pyo", "r") as cache_file:
+      cache = pickle.load(cache_file)
+  except:
+    cache = {}
 
-      if title not in shows.keys():
-        shows[title] = list()
-      shows[title].append((fullpath, destpath, destination))
+  d = Debug(verbose=1)
+  c = DummyConfigParser()
+  e = DummySearcher(debug=d, cache=cache)
+  a = Automover(search_path='.', config=c, searcher=e, debug=d)
+  a.run()
 
-  script = open(args.script, 'a')
-
-  script.write('#!/bin/bash\n\n')
-  script.write('# Shows present in this file:\n# ')
-  script.write('\n# '.join(shows.keys()))
-  script.write('\n\n')
-
-  for show in shows.keys():
-    episodes = sorted(shows[show], key=lambda episode: episode[2])
-    script.write('# %s\n' % show)
-    for fullpath, destpath, destination in episodes:
-      if destpath not in destpathcache:
-        destpathcache[destpath] = False
-
-      if not os.path.isdir(destpath) and not destpathcache[destpath]:
-        script.write('mkdir -p "%s"\n' % destpath)
-        destpathcache[destpath] = True
-      script.write('mv -vb "%s" "%s"\n' % (fullpath, destination))
-    script.write('\n') 
-
-  script.close()
-
-  cachefile = open('caches.pyo', 'w')
-  pickle.dump((showcache, titlecache), cachefile)
-  cachefile.close()
+  with open("cache_file.pyo", "w") as cache_file:
+    pickle.dump(e.cache, cache_file)
