b7a669b668addd19009268fd87fb6b5237a66b08
Sagar Pandya <sagargp@gmail.com>
2014-10-18 17:31:01 -0600

    Finished it up!

--- automover.py
+++ automover.py
@@ -8,13 +8,35 @@ import tvdb_exceptions
 from collections import OrderedDict
 
 class EpisodeFileRepr(object):
-  def __init__(self, path, name):
-    self._log       = logging.getLogger(__name__)
-    self._name      = name
-    self._path      = path
-    self._file_info = None
+  def __init__(self, path, name, transform=False):
+    self._log     = logging.getLogger(__name__)
+    self._name    = name
+    self._path    = path
+
+    # might throw an exception
+    self._file_info = self._get_info(transform)
+
+  def move(self, tvdb_instance, dest, cleanup=None):
+    self._fetch_episode_name(tvdb_instance)
+
+    og_path       = self.get_original_path()
+    dest_dir      = os.path.join(dest, self._file_info["title"], "Season %d" % self._file_info["season"])
+    dest_filename = "%s%s" % (str(self), os.path.splitext(og_path)[-1])
+    dest_file     = os.path.join(dest_dir, dest_filename)
+
+    print '# %s' % str(self)
+    if not os.path.exists(dest_dir):
+      self._log.info("Making destination directory %s" % dest_dir)
+      print 'mkdir -p "%s"' % dest_dir
+    self._log.info("Moving %s to %s" % (og_path, dest_file))
+
+    print 'cp -blv "%s" "%s"' % (og_path, dest_file)
+
+    if cleanup:
+      print 'mv -v "%s" "%s"' % (os.path.join(self._path, self._name), cleanup)
+    print
   
-  def fetch_episode_name(self, tvdb_instance):
+  def _fetch_episode_name(self, tvdb_instance):
     search_results = tvdb_instance.search(self._file_info["title"])
     if not search_results:
       raise tvdb_exceptions.tvdb_shownotfound, "Show not found! Giving up. (%s)" % self._file_info["name"]
@@ -27,23 +49,19 @@ class EpisodeFileRepr(object):
     self._file_info["title"]       = proper_name
     self._file_info["episodename"] = tvdb_instance[proper_name][self._file_info["season"]][self._file_info["episode"]]["episodename"]
   
-  def get_move_command(self, dest):
-    source        = self.find_movie_file()
-    dest_path     = os.path.join(dest, self._file_info["title"], "Season %d" % self._file_info["season"])
-    dest_filename = "%s%s" % (self._file_info["episodename"], os.path.splitext(source)[-1])
-    destination   = os.path.join(dest_path, dest_filename)
-    return 'cp -lbv "{source}" "{destination}"'.format(source=source, destination=destination)
-
-  def get_original_path(self):
-    return os.path.join(self._path, self._name)
-
-  def get_info(self):
-    if self._file_info:
-      return self._file_info
-
+  def _get_info(self, transform=None):
     m = re.match("([\w\.\s]*)(S(\d+)E(\d+)|(\d+)x(\d+))(.*)", self._name)
     if m: 
       title, season_group, s1, e1, s2, e2, _ = m.groups()
+
+      if transform:
+        this, that = transform.split("=")
+        self._log.info("Transforming %s to %s..." % (this, that))
+
+        new_title = title.replace(this, that)
+        self._log.info("%s became %s" % (title, new_title))
+        title = new_title
+
       self._file_info = {"name": self._name, "title": title.replace(".", " ").strip(), "season": None, "episode": None}
       if s1 and e1:
         self._file_info["season"]  = int(s1)
@@ -51,18 +69,17 @@ class EpisodeFileRepr(object):
       elif s2 and e2:
         self._file_info["season"]  = int(s2)
         self._file_info["episode"] = int(e2)
-      self._log.info(str(self))
       return self._file_info
     else:
-      warnings.warn("%s doesn't appear to be a valid TV show" % self._name)
-      return None
+      raise TypeError, "%s doesn't appear to be a valid TV show" % self._name
 
-  def find_movie_file(self):
+  def get_original_path(self):
     path = os.path.join(self._path, self._name)
+    self._log.info("Finding movie file for %s" % path)
     if os.path.isdir(path):
       for f in os.listdir(path):
         if f.lower().endswith(("mkv", "avi", "mp4")) and "sample" not in f.lower():
-          return os.path.join(self._path, f)
+          return os.path.join(path, f)
     else:
       if path.lower().endswith(("mkv", "avi", "mp4")) and "sample" not in path.lower():
         return path
@@ -77,60 +94,56 @@ class EpisodeFileRepr(object):
     return None
 
   def __eq__(self, other):
-    return self._file_info == other._file_info
+    return str(self) == str(other)
 
   def __hash__(self):
     return  hash(str(self))
 
-def find_episode_files(path):
-  episodes = []
-  for f in os.listdir(path):
-    episode = EpisodeFileRepr(path, f)
-    episode.get_info()
-    if episode in episodes:
-      first = episodes.index(episode)
-      if os.stat(episodes[first].find_movie_file()).st_size > os.stat(episode.find_movie_file()).st_size:
-        continue
-    episodes.append(episode)
-  return episodes
+def main(args):
+  log  = logging.getLogger(__name__)
+  TVDB = tvdb_api.Tvdb()
 
-def main():
-  TVDB   = tvdb_api.Tvdb()
-  shopts = OrderedDict()
+  print "#!/bin/bash"
+  print
 
-  # commands
-  shopts['#!/bin/bash']         = None
-  shopts['cd "%s"' % args.path] = None
-
-  # get files
-  all_episodes = find_episode_files(args.path)
-
-  # get all the ep names from TVDB
-  for episode in all_episodes:
-    episode.fetch_episode_name(TVDB)
-    episode.moved = True
-
-  # generate move commands
-  for episode in all_episodes:
-    move_command = episode.get_move_command(args.dest)
-    shopts[move_command] = None
-  
-  # clean up
-  shopts['mkdir -p finished/']  = None
-  for episode in all_episodes:
+  # get all the episodes in args.path
+  all_eps = []
+  for node in os.listdir(args.path):
     try:
-      if episode.moved:
-        shopts['mv -v "%s" finished/' % episode.find_movie_file()] = None
+      e = EpisodeFileRepr(args.path, node, args.transform)
+      og_path = e.get_original_path()
+      log.info("Found %s --> %s" % (str(e), og_path))
+      if e in all_eps:
+        log.info("Detected duplicate!")
+        first = all_eps[all_eps.index(e)]
+        if os.path.getsize(og_path) > os.path.getsize(first.get_original_path()):
+          log.info("Removing the smaller of the two matches")
+          all_eps.remove(e)
+          all_eps.append(e)
+      else:
+        all_eps.append(e)
     except:
       pass
 
-  print "\n".join(shopts.keys())
+  print "# move everything here after we're done"
+  print 'mkdir -p "%s"' % args.cleanup_dir
+  print 
+
+  print "# do the move!"
+
+  # move
+  for episode in all_eps:
+    try:
+      episode.move(TVDB, args.dest, args.cleanup_dir)
+    except tvdb_exceptions.tvdb_shownotfound, e:
+      log.warn(str(e))
 
 if __name__ == "__main__":
   import argparse
   parser = argparse.ArgumentParser()
   parser.add_argument("--verbose", "-v", action="store_true", default=False, help="Print out debug info")
-  parser.add_argument("--force", action="store", help="Assume every file is the show given here")
+  parser.add_argument("--transform", "-t", action="store", help="Must be in the form A=B. Transform A into B before doing TV show detection")
+  parser.add_argument("--cleanup-dir", "-c", action="store", help="Where to move the files after copying them to their destionation", default="./finished")
   parser.add_argument("path", action="store", help="The path to the root of all the files that need to be renamed")
   parser.add_argument("dest", action="store", help="The path to destination of the files after they've been renamed")
   args = parser.parse_args()
@@ -142,4 +155,4 @@ if __name__ == "__main__":
   if not args.verbose:
     log.disabled = True
 
-  main()
+  main(args)
